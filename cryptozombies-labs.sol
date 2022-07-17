0d3053db2818b590b49932e8af689aae78162de2
marcosptf <marcosptf@yahoo.com.br>
2022-07-17 19:16:02 -0300

    Update cryptozombies-labs.sol

--- cryptozombies-labs.sol
+++ cryptozombies-labs.sol
@@ -15,8 +15,10 @@ contract ZombieFactory {
         uint dna;
     }
 
-    //aqui estamos declarando uma variavel array
-    //do tipo structs de acesso public
+    /*
+    * aqui estamos declarando uma variavel array
+    * do tipo structs de acesso public
+    */
     Zombie[] public zombies;
 
     /**
@@ -33,5 +35,27 @@ contract ZombieFactory {
     function _createZombie(string _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
+    
+    /**
+    * Modificadores das Funções
+    * Na verdade a função acima não altera estado algum em Solidity 
+    * em outras palavras não altera qualquer valor ou escreve qualquer coisa.
+    * Neste caso nós podemos declará-la como uma função com palavra reservada 
+    * view (observa), que significa: somente uma observação do dado, mas nenhuma alteração de fato.
+    */
+    function sayHello() public view returns (string) { }
+    
+    /**
+    * Em Solidity também existem funções puras usando a palavra reservada pure, 
+    * que significa que nenhum dado será acessado na aplicação. Pense na seguinte situação:
+    * Esta função nem mesmo lê um estado da aplicação - os seus valores retornados 
+    * dependem somente dos parâmetros da função. Então neste caso nós podemos declarar a 
+    * função como pura usando a palavra reservada pure.
+    */
+    function _multiply(uint a, uint b) private pure returns (uint) {
+      return a * b;
+    }
+    
+    
 
 }
