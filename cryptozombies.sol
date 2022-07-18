b7c0dd345b82dcac49409b3bd8c121f671a21688
marcosptf <marcosptf@yahoo.com.br>
2022-07-18 12:19:56 -0300

    Update cryptozombies.sol

--- cryptozombies.sol
+++ cryptozombies.sol
@@ -16,11 +16,13 @@ contract ZombieFactory {
 
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
-    
+
     function _createZombie(string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) -1;
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
@@ -28,8 +30,11 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
+contract ZombieFeeding is ZombieFactory {}
