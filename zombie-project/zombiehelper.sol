5b7227ef6f8c0e7a0359a091935c9b12a82bb5fc
marcosptf <marcosptf@yahoo.com.br>
2022-08-05 13:26:12 -0300

    Update zombiehelper.sol

--- zombie-project/zombiehelper.sol
+++ zombie-project/zombiehelper.sol
@@ -4,9 +4,16 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
@@ -22,14 +29,12 @@ contract ZombieHelper is ZombieFeeding {
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
     uint counter = 0;
-
-    for (uint i=0; i < zombies.length; i++) {
+    for (uint i = 0; i < zombies.length; i++) {
       if (zombieToOwner[i] == _owner) {
         result[counter] = i;
         counter++;
       }
     }
-
     return result;
   }
 
