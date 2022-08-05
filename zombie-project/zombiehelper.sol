77c5cb8766460d058d9d858d43de8c4e119a7fe8
marcosptf <marcosptf@yahoo.com.br>
2022-08-05 06:57:30 -0300

    Create zombiehelper.sol

---
+++ zombie-project/zombiehelper.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    //esta linha abaixo faz com que ele continue
+    //executando o resto da function modificada;
+    _;
+  }
+
+}
