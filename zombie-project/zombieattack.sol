534d2faf112f1239bdf87cbf340668b469e704e8
marcosptf <marcosptf@yahoo.com.br>
2022-08-05 16:05:36 -0300

    Update zombieattack.sol

--- zombie-project/zombieattack.sol
+++ zombie-project/zombieattack.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.19;
+
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
@@ -9,6 +11,19 @@ contract ZombieBattle is ZombieHelper {
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  function attack(uint _zombieId, uint _targetId) external {
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } else {
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
   }
 }
